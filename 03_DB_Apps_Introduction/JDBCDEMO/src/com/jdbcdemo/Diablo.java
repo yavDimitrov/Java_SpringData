20485c8b9c25a5a06257244e89943ddb39bdbe08
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-22 11:25:47 +0300

    03

--- 03_DB_Apps_Introduction/JDBCDEMO/src/com/jdbcdemo/Diablo.java
+++ 03_DB_Apps_Introduction/JDBCDEMO/src/com/jdbcdemo/Diablo.java
@@ -1,8 +1,6 @@
 package com.jdbcdemo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -16,6 +14,29 @@ public class Diablo {
 
         Connection connection = DriverManager
                  .getConnection("jdbc:mysql:://localhost:3306/diablo", properties);
+
+        System.out.println("Enter username: ");
+        String username = scan.nextLine();
+
+        PreparedStatement query = connection.prepareStatement("SELECT user_name, first_name, last_name, COUNT(ug.id)" +
+                "FROM users" +
+                "JOIN users_games AS ug ON users.id = ug.user_id" +
+                "WHERE user_name = ?");
+
+        query.setString(1, username);
+
+        ResultSet result = query.executeQuery();
+        String dbUsername = result.getString("user_name");
+        String dbFirstName = result.getString("first_name");
+        String dbLastName = result.getString("last_name");
+        int dbCountGames = result.getInt("COUNT(ug.id)");
+
+        if(result.next()) {
+            System.out.printf("User: %s\n" +
+                    "%s %S has played %d games", dbUsername, dbFirstName, dbLastName, dbCountGames);
+        } else {
+            System.out.println("No such user exists");
+        }
 
     }
 }

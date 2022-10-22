d82000a5542e56701bc853c6614ae4ac85547a7d
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-22 17:32:52 +0300

    Constraints.java

---
+++ 03_DB_Apps_Introduction/DBAppsExercises/DBAppsExercises/src/Constraints.java
@@ -0,0 +1,10 @@
+enum Constraints {
+    ;
+    static final String USER_KEY = "user";
+    static final String USER_VALUE = "root";
+
+    static final String PASSWORD_KEY = "password";
+    static final String PASSWORD_VALUE = "";
+
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/minions_db";
+}

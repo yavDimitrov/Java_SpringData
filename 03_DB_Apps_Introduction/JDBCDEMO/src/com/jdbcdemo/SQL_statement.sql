f7ae951d7ea68ecbbeb7476216cda76a97a75974
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-22 14:56:39 +0300

    SQL update to no rows

--- 03_DB_Apps_Introduction/JDBCDEMO/src/com/jdbcdemo/SQL_statement.sql
+++ 03_DB_Apps_Introduction/JDBCDEMO/src/com/jdbcdemo/SQL_statement.sql
@@ -2,3 +2,7 @@ SELECT user_name, first_name, last_name, COUNT(ug.id)
 FROM users
 JOIN users_games AS ug ON users.id = ug.user_id
 WHERE user_name = 'nakov';
+
+SELECT user_name, first_name, last_name, (SELECT COUNT(*) FROM users_games WHERE user_id = u.id)
+FROM users AS u
+WHERE user_name = 'nakov'; 

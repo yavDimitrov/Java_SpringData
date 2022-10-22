1c2a7ef44306fbcf5974224182b61268fb0b1438
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-22 17:44:21 +0300

    order

--- 03_DB_Apps_Introduction/DBAppsExercises/DBAppsExercises/src/mySQL.sql
+++ 03_DB_Apps_Introduction/DBAppsExercises/DBAppsExercises/src/mySQL.sql
@@ -2,3 +2,9 @@ SELECT * FROM minions as m
                   JOIN minions_villains mv on m.id = mv.minion_id;
 
 
+SELECT v.name, count(distinct mv.minion_id) AS minions_count
+FROM villains AS v
+    JOIN minions_villains AS mv ON v.id = mv.villain_id
+GROUP BY mv.villain_id
+HAVING minions_count > 15
+ORDER BY minions_count;

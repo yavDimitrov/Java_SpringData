f29b89749fc5b5b681f192fbf28686c268a049ee
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-25 08:20:43 +0300

    Excercises

---
+++ 03_DB_Apps_Introduction/DBAppsExercises/DBAppsExercises/out/production/DBAppsExercises/mySQL.sql
@@ -0,0 +1,28 @@
+SELECT * FROM minions as m
+                  JOIN minions_villains mv on m.id = mv.minion_id;
+
+
+SELECT v.name, count(distinct mv.minion_id) AS minions_count
+FROM villains AS v
+    JOIN minions_villains AS mv ON v.id = mv.villain_id
+GROUP BY mv.villain_id
+HAVING minions_count > 15
+ORDER BY minions_count;
+
+
+SELECT m.name, m.age
+FROM minions AS m
+JOIN minions_villains mv on m.id = mv.minion_id
+WHERE mv.villain_id = 1
+
+SELECT * FROM villains AS v
+WHERE v.id = ?
+
+
+04____
+
+SELECT * FROM villains
+WHERE villains.name = ?;
+
+SELECT t.id FROM towns AS t
+WHERE t.name = ?;

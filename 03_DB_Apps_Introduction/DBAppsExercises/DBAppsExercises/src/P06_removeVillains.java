09c975fcd8d5a3f6ca2830c3fcbec38542836371
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-26 08:26:16 +0300

    P06_Start

---
+++ 03_DB_Apps_Introduction/DBAppsExercises/DBAppsExercises/src/P06_removeVillains.java
@@ -0,0 +1,8 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class P06_removeVillains {
+    public static void main(String[] args) throws SQLException {
+        final Connection connection = Utils.getSQLConnection();
+    }
+}

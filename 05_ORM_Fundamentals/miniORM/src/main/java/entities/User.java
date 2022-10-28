07531c4bc14397492f36ce2a8c409e7a447c7e84
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-10-29 01:04:36 +0300

    Part01

--- 05_ORM_Fundamentals/miniORM/src/main/java/entities/User.java
+++ 05_ORM_Fundamentals/miniORM/src/main/java/entities/User.java
@@ -1,4 +1,55 @@
 package entities;
 
+import java.time.LocalDate;
+
 public class User {
+
+    private long id;
+
+    private String username;
+
+    private int age;
+
+    private LocalDate registration;
+
+
+    public User(String username, int age, LocalDate registration) {
+        this.username = username;
+        this.age = age;
+        this.registration = registration;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(LocalDate registration) {
+        this.registration = registration;
+    }
 }
+
+

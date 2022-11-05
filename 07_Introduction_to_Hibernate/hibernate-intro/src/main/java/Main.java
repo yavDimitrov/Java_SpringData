73de22713fefa8bf73059a6d05a54ba890109c55
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-11-05 18:34:05 +0200

    03

--- 07_Introduction_to_Hibernate/hibernate-intro/src/main/java/Main.java
+++ 07_Introduction_to_Hibernate/hibernate-intro/src/main/java/Main.java
@@ -3,6 +3,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -20,8 +22,18 @@ public class Main {
 
         */
 
-        Student fromDB = session.get(Student.class, 0);
+/*        Student fromDB = session.get(Student.class, 0);
         System.out.println(fromDB.getId() + " "  + fromDB.getName());
+
+        */
+
+        List<Student> students =
+                session.createQuery("FROM Student", Student.class)
+                        .list();
+
+        for (Student student : students) {
+            System.out.println(student.getId() + " " + student.getName());
+        }
 
         session.getTransaction().commit();
         session.close();

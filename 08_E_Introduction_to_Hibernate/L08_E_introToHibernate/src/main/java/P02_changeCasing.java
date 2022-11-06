2d5c7c5579842836250f00dd94aee6e709c743f8
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-11-06 11:10:52 +0200

    02

--- 08_E_Introduction_to_Hibernate/L08_E_introToHibernate/src/main/java/P02_changeCasing.java
+++ 08_E_Introduction_to_Hibernate/L08_E_introToHibernate/src/main/java/P02_changeCasing.java
@@ -1,2 +1,38 @@
+import entities.Town;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class P02_changeCasing {
+    private static final String DATABASE_NAME = "soft_uni";
+/*    private static final String UPDATE_ALL_TOWNS_WITH_LENGHT_NAME_MORE_THAN_5
+            = "UPDATE towns t SET t.name = UPPER(t.name) WHERE LENGTH(t.name) > 5";*/
+    public static void main(String[] args) {
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(DATABASE_NAME);
+
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+       final Query towns = entityManager.createQuery("SELECT t FROM Town t", Town.class);
+
+       final List<Town> resultList = towns.getResultList();
+
+        for (Town town : resultList) {
+            final String townName = town.getName();
+
+            if(townName.length() <= 5) {
+                town.setName(townName.toUpperCase());
+                entityManager.persist(town);
+            }
+            
+        }
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 }

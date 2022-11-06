2eabe46f71c8708809bc77df7f60d59d50852034
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-11-06 10:08:35 +0200

    2022-11-06_Excersises to hibernate

---
+++ 08_E_Introduction_to_Hibernate/L08_E_introToHibernate/src/main/java/P02_changeCasing.java
@@ -0,0 +1,2 @@
+public class P02_changeCasing {
+}
---
+++ 08_E_Introduction_to_Hibernate/L08_E_introToHibernate/src/main/java/entities/Town.java
@@ -0,0 +1,34 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "towns")
+public class Town {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "town_id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

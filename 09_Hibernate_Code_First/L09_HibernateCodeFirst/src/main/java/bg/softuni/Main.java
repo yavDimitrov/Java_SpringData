e3f9f19ac3a462167dbf7ce4b509889eadcd66e2
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-11-11 22:45:38 +0200

    2022-11-11

--- 09_Hibernate_Code_First/L09_HibernateCodeFirst/src/main/java/bg/softuni/Main.java
+++ 09_Hibernate_Code_First/L09_HibernateCodeFirst/src/main/java/bg/softuni/Main.java
@@ -1,5 +1,6 @@
 package bg.softuni;
 
+
 import bg.softuni.entities.Bike;
 import bg.softuni.entities.Car;
 import bg.softuni.entities.Plane;
@@ -9,23 +10,46 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Main {
+
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence
-                .createEntityManagerFactory("relations");
+                .createEntityManagerFactory("relations123");
         EntityManager entityManager = emf.createEntityManager();
         entityManager.getTransaction().begin();
 
         Vehicle car = new Car("Ford Focus", "Petrol", 5);
         Vehicle bike = new Bike();
-        Vehicle plane = new Plane("AirBus", "Petrol", 200);
+        Vehicle plane = new Plane("Airbus", "Petrol", 200);
 
         entityManager.persist(car);
         entityManager.persist(bike);
         entityManager.persist(plane);
+//
+//        PlateNumber number = new PlateNumber("123");
+//        Truck truck1 = new Truck(number);
+//        Truck truck2 = new Truck(number);
+//
+//        entityManager.persist(number);
+//        entityManager.persist(truck1);
+//        entityManager.persist(truck2);
+//
+////        Car fromDB = entityManager.find(Car.class, 1L);
+//
+////        System.out.println(fromDB.getSeats() + " " + fromDB.getModel());
 
+/*        Article article = new Article("alabala");
+        User author = new User("Toshko");
+
+        author.addArtice(article);
+        article.setAuthor(author);
+
+        entityManager.persist(author);
+
+        User user = entityManager.find(User.class, 2);
+
+        System.out.println();*/
 
         entityManager.getTransaction().commit();
         entityManager.close();
-
     }
 }

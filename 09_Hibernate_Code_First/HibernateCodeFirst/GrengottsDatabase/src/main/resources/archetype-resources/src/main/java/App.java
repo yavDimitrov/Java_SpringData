4d342106c54a20135923ce7cd3fd2701909c591b
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-11-13 23:51:28 +0200

    05

---
+++ 09_Hibernate_Code_First/HibernateCodeFirst/GrengottsDatabase/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $bg.softuni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

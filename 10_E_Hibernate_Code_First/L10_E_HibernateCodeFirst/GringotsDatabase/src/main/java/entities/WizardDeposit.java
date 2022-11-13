f78e378aaf1dd6ef0ba0f64bc36919f51e26aa33
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-11-14 00:19:23 +0200

    L10_02

--- 10_E_Hibernate_Code_First/L10_E_HibernateCodeFirst/GringotsDatabase/src/main/java/entities/WizardDeposit.java
+++ 10_E_Hibernate_Code_First/L10_E_HibernateCodeFirst/GringotsDatabase/src/main/java/entities/WizardDeposit.java
@@ -1,4 +1,61 @@
 package entities;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class WizardDeposit {
+
+    @Id
+    private long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String notes;
+
+    @Column
+    private int age;
+
+    @Column
+    private String magic_wand_creator;
+
+    @Column
+    private String magic_wand_size;
+
+    @Column
+    private String deposit_group;
+
+    @Column
+    private String deposit_startData;
+
+    @Column
+    private String deposit_amount;
+
+    @Column
+    private String deposit_interest;
+
+    @Column
+    private String deposit_charge;
+
+    @Column
+    private String deposit_expiration_date;
+
+    @Column
+    private String is_deposit_expired;
+
+
 }

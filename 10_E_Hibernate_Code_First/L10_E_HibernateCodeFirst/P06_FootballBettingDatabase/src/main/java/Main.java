8c750ad67aad6b5094d3c8ef1303e9b4b961b93f
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-11-20 01:35:11 +0200

    2022-11-20_01

--- 10_E_Hibernate_Code_First/L10_E_HibernateCodeFirst/P06_FootballBettingDatabase/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.persistence.Persistence;
-
-public class Main {
-    public static void main(String[] args) {
-
-          Persistence.createEntityManagerFactory("soft_uni").createEntityManager();
-
-    }
-}

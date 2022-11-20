9d4c09a15628801ac7d24cd2bf654355a774db96
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-11-20 23:22:13 +0200

    2022-11-20_02

---
+++ 11_Spring_Data_Intro/L11_Spring_Data_Intro/src/main/java/bg/softuni/l11_spring_data_intro/L11SpringDataIntroApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.l11_spring_data_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L11SpringDataIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L11SpringDataIntroApplication.class, args);
+    }
+
+}
---
+++ 11_Spring_Data_Intro/L11_Spring_Data_Intro/src/test/java/bg/softuni/l11_spring_data_intro/L11SpringDataIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.l11_spring_data_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L11SpringDataIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

22e0bde48c9cb29dd46a4703381c96b3df5dd8c5
Yavor Dimitrov <98236929+yavDimitrov@users.noreply.github.com>
2022-12-19 23:25:12 +0200

    Add files via upload
    2022-12-19

---
+++ 15_Spring_Data_Auto_Mapping_Objects/ModelMapper/src/main/java/bg/softuni/modelmapper/ModelMapperMain.java
@@ -0,0 +1,56 @@
+package bg.softuni.modelmapper;
+
+import bg.softuni.modelmapper.entities.Address;
+import bg.softuni.modelmapper.entities.Employee;
+import bg.softuni.modelmapper.entities.dtos.EmployeeDTO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class ModelMapperMain implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+        ModelMapper mapper = new ModelMapper();
+
+/*        PropertyMap<Employee, EmployeeDTO> propertyMap = new PropertyMap<Employee, EmployeeDTO>() {
+            @Override
+            protected void configure() {
+                map().setCity(source.getAddress().getCity());
+            }
+        };
+        mapper.addMappings(propertyMap);*/
+
+        TypeMap<Employee, EmployeeDTO> typeMap = mapper.createTypeMap(Employee.class, EmployeeDTO.class);
+        /*
+                typeMap.addMappings(mappings -> mappings.map
+                (source -> source.getAddress().getCity(),
+                        (destination, value) -> destination.setCity(value.toString())));
+
+                 typeMap.validate();
+        */
+
+        typeMap.addMappings(mappings -> mappings.map
+                (source -> source.getAddress().getCity(),
+                       EmployeeDTO::setCity));
+
+
+
+        typeMap.validate();
+
+
+
+
+        Address address = new Address("Bulgaria", "Sofia");
+        Employee employee = new Employee("First", BigDecimal.TEN, address);
+
+        EmployeeDTO employeeDTO = typeMap.map(employee);
+
+        System.out.println(employeeDTO);
+
+    }
+}
--- 15_Spring_Data_Auto_Mapping_Objects/ModelMapper/src/main/java/bg/softuni/modelmapper/entities/Employee.java
+++ 15_Spring_Data_Auto_Mapping_Objects/ModelMapper/src/main/java/bg/softuni/modelmapper/entities/Employee.java
@@ -1,20 +1,31 @@
 package bg.softuni.modelmapper.entities;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "employees")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(nullable = false)
     private BigDecimal salary;
 
+    @Column(nullable = false)
     private LocalDate birthday;
 
+    @ManyToOne(optional = false)
     private Address address;
 
     public Employee () {}
